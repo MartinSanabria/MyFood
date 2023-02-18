9ebc53cee3bde8dcf5bbd7dd76652d4c30d1f82a
jose martin sanabria flores <martin.sanabria@creativaconsultores.com>
2023-02-17 22:54:12 -0600

    nuevos cambios

--- views/Food/index.php
+++ views/Food/index.php
@@ -1,8 +1,22 @@
 <?php require_once VIEW_LAYOUT_PATH . 'header.php' ?>
-
+<link rel="stylesheet" href="<?= URL . "public/css/food.css" ?>">
 <!-- INSERTAR CONTENIDO -->
-
-
+<body>
+    <section class="container container-food">
+        <div class="card-content">
+            <img src="/MyFood/public/img/pupusa.jpg" class="card-img-top" alt="...">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolores.</p>
+            </div>
+        </div>
+        <div class="card-content">
+            <img src="/MyFood/public/img/drink.jpg" class="card-img-top" alt="...">
+            <div class="content">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, dolores.</p>
+            </div>
+        </div>
+    </section>
+</body>
 <!-- FIN CONTENIDO -->
 
 <?php require_once VIEW_LAYOUT_PATH . "footer.php" ?>
--- views/Home/nosotros.php
+++ views/Home/nosotros.php
@@ -1,9 +1,11 @@
 <?php require_once VIEW_LAYOUT_PATH . 'header.php' ?>
 
+
 <!-- INSERTAR CONTENIDO -->
-<div class="container">
-    <h1>Nosotros</h1>
-</div>
+<body>
+
+</body>
+
 <!-- FIN CONTENIDO -->
 
 <?php require_once VIEW_LAYOUT_PATH . "footer.php" ?>
